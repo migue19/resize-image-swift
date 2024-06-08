ea6acbc8644c816140be5e6fda168d34da9b3e84
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2024-06-08 09:51:24 -0600

    Initial Commit

---
+++ ResizeImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ResizeImage
+//
+//  Created by Miguel Mexicano Herrera on 08/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
